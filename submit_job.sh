f83407363a4070e680514c2ff4e7bf7e8484991d
Manyou Ma <manyouma@gmail.com>
2022-06-22 15:26:35 -0700

    Update submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 #SBATCH --account=rrg-vincentw
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=128000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=00-24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=00-1:00:00
 #SBATCH --job-name=jun22_test
 #SBATCH --output=%x-%j.out
 
 module load python/3.8.10
-#source /home/manyouma/projects/def-vincentw/manyouma/WhittleLearn/new_env/bin/activate
+module load scipy-stack
 
-
-# if gpu is to be used
 python parallel_VI.py
 
 
